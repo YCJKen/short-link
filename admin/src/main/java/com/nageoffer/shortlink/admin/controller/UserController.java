63eaaace42a9bd2dc0d6a7f93a7f4f014a2ee8a1
Yucheng Jiang <112424374+315462438@users.noreply.github.com>
2024-05-18 17:09:06 -0500

    admin change
    设置目标脱敏功能， errorcode & errormsg define（enum classes）, 设置result返回实体类, 设置全局异常拦截器（拦截异常，并返回result）

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
 import com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum;
+import com.nageoffer.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,11 +31,11 @@ public class UserController {
     * */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByName(@PathVariable("username") String username){
-        UserRespDTO result = userService.getUserByUsername(username);
-        if(result == null){
-            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
-        }else {
-            return Results.success(result);
-        }
+        return Results.success(userService.getUserByUsername(username));
+    }
+
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByName(@PathVariable("username") String username){
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
 }
